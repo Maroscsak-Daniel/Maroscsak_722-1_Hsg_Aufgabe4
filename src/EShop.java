283e834c523b6125152c7a76f91f115a41b0d94c
Maroscsak Daniel <daniel.maroscsak@gmail.com>
2025-10-20 16:10:37 +0300

    Ub 4 subp 2

--- src/EShop.java
+++ src/EShop.java
@@ -1,9 +1,11 @@
 public class EShop {
 
     public int[] tastatur;
+    public int[] usb;
 
-    public EShop(int[] tastatur) {
+    public EShop(int[] tastatur,  int[] usb) {
         this.tastatur = tastatur;
+        this.usb = usb;
     }
 
     public int billigsteTastatur() {
@@ -14,6 +16,21 @@ public class EShop {
             }
         }
         return min;
+    }
+
+    public int teuerstenGegenstand() {
+        int max = Integer.MIN_VALUE;
+        for (int i=0; i < usb.length; i++) {
+            if (usb[i] > max) {
+                max = usb[i];
+            }
+        }
+        for  (int i=0; i < tastatur.length; i++) {
+            if (tastatur[i] > max) {
+                max = tastatur[i];
+            }
+        }
+        return max;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        EShop tastaturen = new EShop(new int[] {40,35,70,15,45});
-        System.out.println(tastaturen.billigsteTastatur());
+        EShop gerate = new EShop(new int[] {15, 20, 10, 35}, new int[] {20, 15, 40, 15});
+        System.out.println(gerate.billigsteTastatur());
+        System.out.println(gerate.teuerstenGegenstand());
     }
 }
